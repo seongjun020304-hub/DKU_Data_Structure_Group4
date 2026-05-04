b6e0bc49c8c6d5e06c68fa7ab2e351a9277e55ef
SJC222 <seongjun020304@gmail.com>
2026-05-05 00:58:48 +0900

    Create CircularQueue.h

---
+++ Linked list, Stack, Queue/include/CircularQueue.h
@@ -0,0 +1,27 @@
+#ifndef CIRCULAR_QUEUE_H
+#define CIRCULAR_QUEUE_H
+
+// 상수 및 구조체 선언
+const int MAX_SIZE = 21;
+
+struct CircularQueue {
+    char data[MAX_SIZE];
+    int  front;
+    int  rear;
+    int  capacity;
+};
+
+// 함수 선언(껍데기)만 작성
+void initQueue(CircularQueue& q);
+bool isEmpty(const CircularQueue& q);
+bool isFull(const CircularQueue& q);
+int size(const CircularQueue& q);
+void enqueue(CircularQueue& q, char c);
+char dequeue(CircularQueue& q);
+char peek(const CircularQueue& q);
+void printQueue(const CircularQueue& q);
+
+// 기존 main() 함수를 대체할 실행 함수
+void runProblem3();
+
+#endif
