53f0f92b88b9eb4ec43c6c13834ded6eb7710dbd
SJC222 <seongjun020304@gmail.com>
2026-05-05 14:31:47 +0900

    Create problem1.h

---
+++ Linked list, Stack, Queue/include/problem1.h
@@ -0,0 +1,15 @@
+#ifndef PROB1_H
+#define PROB1_H
+
+// 1. 함수의 '껍데기(선언)'만 둡니다! (알맹이는 .cpp 파일에)
+double f(double x);
+
+// 2. 나머지 함수들도 선언만 잘 해주셨습니다.
+void plotGraph(double xMin, double xMax, int samples);
+void findRoots(double xMin, double xMax, int steps);
+void findMinimum(double xMin, double xMax);
+
+// 3. 실행 함수 (int 대신 void로 통일)
+void Problem1();
+
+#endif
