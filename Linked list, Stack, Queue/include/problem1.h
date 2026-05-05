18799785245902546101dfa8492643433b13ca0a
SJC222 <seongjun020304@gmail.com>
2026-05-05 14:52:57 +0900

    Update problem1.h

--- Linked list, Stack, Queue/include/problem1.h
+++ Linked list, Stack, Queue/include/problem1.h
@@ -1,10 +1,10 @@
 #ifndef PROB1_H
 #define PROB1_H
 
-// 1. 함수의 '껍데기(선언)'만 둡니다! (알맹이는 .cpp 파일에)
+// 변수 작성
 double f(double x);
 
-// 함수 선언.
+// 함수 선언 작성
 void plotGraph(double xMin, double xMax, int samples);
 void findRoots(double xMin, double xMax, int steps);
 void findMinimum(double xMin, double xMax);
